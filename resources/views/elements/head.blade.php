5537316919966ace0d9296dbe885fe75aa5e28e2
steeven-js <jacques.steeven@gmail.com>
2023-07-06 14:57:14 -0400

    wip

--- resources/views/elements/head.blade.php
+++ resources/views/elements/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'flowbite-JS') }}</title>
+    <title>{{ config('app.name', 'web-JS') }}</title>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
 
