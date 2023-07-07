b824f646ef88f65577dc8a5e740680dadde86630
steeven-js <jacques.steeven@gmail.com>
2023-07-07 10:32:10 -0400

    Content

--- resources/views/2-hero/hero.blade.php
+++ resources/views/2-hero/hero.blade.php
@@ -1,29 +1,32 @@
-<section class="bg-white dark:bg-gray-900 h-[calc(100vh-4rem)]" id="Home">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
+<section class=" bg-white dark:bg-gray-900" id="Home">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+        <div class="flex flex-col justify-center">
             <h1
-                class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-                Payments tool for software companies</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                From
-                checkout to global sales tax compliance, companies around the world use Flowbite to simplify their
-                payment stack.</p>
-            <a href="#"
-                class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Get started
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </a>
-            <a href="#"
-                class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Sales
-            </a>
+                class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                Votre concepteur d'applications de confiance</h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Simplifiez votre vie grâce à des applications sur mesure.
+            </p>
+            <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+                <a href="#Pricing"
+                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                    Get started
+                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                </a>
+                <a href="https://portfolio.jsprod.fr/" target="_blank"
+                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    Voir plus
+                </a>
+            </div>
         </div>
-        <div class="lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
+        <div>
+            <video class="w-full" autoplay controls>
+                <source src="{{asset('storage/video/video.mp4')}}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
+    </div>
 </section>
--- resources/views/3-about/about.blade.php
+++ resources/views/3-about/about.blade.php
@@ -1,15 +1,14 @@
 <section class="bg-white dark:bg-gray-800" id="About">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't
-                reinvent
-                the wheel</h2>
-            <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small
-                enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.
-                Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you
-                need.</p>
-            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be
-                simple and quick.</p>
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Applications uniques et fonctionnelles : Mon expertise à votre service</h2>
+            <p class="mb-4">"Découvrez l'art de la conception d'applications mobiles et web, où l'innovation rencontre
+                l'expérience utilisateur. Je mets mon expertise et ma créativité au service de la création
+                d'applications uniques et fonctionnelles. Des interfaces intuitives aux fonctionnalités puissantes,
+                chaque élément est minutieusement conçu pour offrir une expérience utilisateur exceptionnelle."
+            </p>
+            <p>"Votre entreprise mérite une présence digitale impactante. En tant que concepteur expérimenté, je crée des applications sur mesure qui répondent parfaitement à vos besoins. Avec une approche centrée sur l'utilisateur et une expertise technique solide, je façonne des applications qui captivent votre audience, renforcent votre marque et stimulent votre croissance."
+            </p>
         </div>
         <div class="grid grid-cols-2 gap-4 mt-8">
             <img class="w-full rounded-lg"
